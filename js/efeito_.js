4d7972f97f67029550c208b8980f76e4fa453b4d
alex sandro <alexsandropimenta16@gmail.com>
2025-05-18 13:17:33 -0300

    colocando os texto e imagens no site do cliente

---
+++ js/efeito_.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('nav ul');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active'); // transforma o botão em X
+  menu.classList.toggle('open');     // abre/fecha o menu
+});
+
