5f7500e221ddbdc7f58753283f1c9ecccff4a764
Alex Sandro <alexsandropimenta16@gmail.com>
2025-07-11 17:01:43 -0300

    google analitic intregrado no site

--- app/views/templates/head.php
+++ app/views/templates/head.php
@@ -1,17 +1,30 @@
  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="#" type="image/x-icon">
-    <link rel="shortcut icon" href="/assets/img/Logo_Cultura.png" type="image/x-icon">
-    <link rel="stylesheet" href="/assets/css/reset.css">
-   <link rel="stylesheet" href="/assets/css/style.css?v=<?= time(); ?>">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="shortcut icon" href="#" type="image/x-icon">
+ <link rel="shortcut icon" href="/assets/img/Logo_Cultura.png" type="image/x-icon">
+ <link rel="stylesheet" href="/assets/css/reset.css">
+ <link rel="stylesheet" href="/assets/css/style.css?v=<?= time(); ?>">
 
 
-    <link rel="stylesheet" type="text/css" href="assets/js/slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="assets/js/slick/slick-theme.css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
+ <link rel="stylesheet" type="text/css" href="assets/js/slick/slick.css" />
+ <link rel="stylesheet" type="text/css" href="assets/js/slick/slick-theme.css" />
+ <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+ <link rel="stylesheet" type="text/css"
+     href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
+
+ <!-- Google tag (gtag.js) -->
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZF40SL361Y"></script>
+ <script>
+     window.dataLayer = window.dataLayer || [];
+
+     function gtag() {
+         dataLayer.push(arguments);
+     }
+     gtag('js', new Date());
+
+     gtag('config', 'G-ZF40SL361Y');
+ </script>
 
 
-    <title>Cultura Efatá</title>
+ <title>Cultura Efatá</title>
